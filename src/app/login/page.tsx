b929adcd723959bd4b46ad67a350e3a442813fd5
sonjeet-kaur <gnisonjeet@gmail.com>
2025-06-24 11:25:03 +0530

    df

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Login() {
+    return (
+        <div>
+            <p>
+                Login Page
+            </p>
+            <label>Username</label>
+            <input type="text" placeholder="Enter Username" />
+        </div>
+    )
+}
