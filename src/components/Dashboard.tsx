1e86b0f17657edeeb01683c14b94b1bf81dd973e
rohitgoyal345 <rhogoyal2005@gmail.com>
2025-07-04 16:20:06 +0530

    up

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,7 +3,7 @@
 import '@/Assets/Style/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import _fetch from '@/config/api';
-import '@/components/Header';
+import Header from '@/components/Header';
 
 export default function Dashboard(pageTitle: any) {
 
@@ -11,7 +11,7 @@ export default function Dashboard(pageTitle: any) {
 
     return (
     <section className='main'>
-        <header />
+        <Header />
         <h6>main</h6>
     </section>
     )
