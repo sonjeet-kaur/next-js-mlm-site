9fed8e224c076f37f28f0c3676c8245790a0f382
rohitgoyal345 <rhogoyal2005@gmail.com>
2025-07-04 16:09:39 +0530

    new

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,6 +3,7 @@
 import '@/Assets/Style/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import _fetch from '@/config/api';
+import '@/components/Header';
 
 export default function Dashboard(pageTitle: any) {
 
@@ -10,6 +11,7 @@ export default function Dashboard(pageTitle: any) {
 
     return (
     <section className='main'>
+        <header />
         <h6>main</h6>
     </section>
     )
